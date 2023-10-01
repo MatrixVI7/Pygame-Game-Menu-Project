6a8be0fc66299efe06aa15cd46e34349bacd98f3
MatrixVI7 <146598404+MatrixVI7@users.noreply.github.com>
2023-10-01 14:49:54 +0200

    Add files via upload

--- python1.py
+++ python1.py
@@ -1,15 +1,28 @@
 import pygame
+#############################
+#                                                     #
+#############################
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
+screenwidth = 800
+screenheight = 600
+
+screen = pygame.display.set_mode((screenwidth, screenheight))
 clock = pygame.time.Clock()
 running = True
-dt = 0
+
 
 pygame.display.set_caption("TEST PROJECT")
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height()/2)
+x = 50
+y=  40
+width = 40
+height = 60
+vel = 5
+
+isJump = False
+jumpCount = 10
 
 while running:
     for event in pygame.event.get():
@@ -18,18 +31,29 @@ while running:
     screen.fill((0,0,0))
 
 
-    pygame.draw.circle(screen, (255,0,0), player_pos, 40)
+    pygame.draw.rect(screen, (255,0,0), (x, y, width, height))
 
     keys = pygame.key.get_pressed()
-    
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+    if not(isJump):
+        
+        if keys[pygame.K_w] and y > 0:
+            y -= vel
+        if keys[pygame.K_s] and y < 600 - height:
+            y += vel
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+            if jumpCount >= -10:
+                y -= (jumpCount * abs(jumpCount)) * 0.5
+                jumpCount -= 1
+            else:
+                jumpCount = 10
+                isJump = False
+                
+    if keys[pygame.K_a] and x > 0:
+        x -= vel
+    if keys[pygame.K_d] and x < screenwidth - width:
+        x += vel
 
     pygame.display.update()
     dt = clock.tick(60) / 1000
